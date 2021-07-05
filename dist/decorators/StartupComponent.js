3efe51be954347ff3b944aee9f850c0373a6a5d0
AntoRin_ux <antorinish@gmail.com>
2021-07-05 17:31:23 +0530

    remove build

--- dist/decorators/StartupComponent.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.startupComponent = void 0;
-require("reflect-metadata");
-function startupComponent(target, key, _) {
-    Reflect.defineMetadata("startupComponent", true, target, key);
-}
-exports.startupComponent = startupComponent;
-//# sourceMappingURL=StartupComponent.js.map
